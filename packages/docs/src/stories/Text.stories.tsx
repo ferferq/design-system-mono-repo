7961673ed2437ba7c78f5c471419f68422fb6a54
ferferq <fernandoalvesq@gmail.com>
2023-02-10 16:22:31 -0300

    design system mono repo

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from '@fmartins-ignite-ui/react';
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, eligendi vitae provident quas cupiditate excepturi accusantium quaerat libero animi repellat voluptatum porro, ipsa eius at. Autem perferendis praesentium dolorum voluptatem!',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>;
+
+export const Primary: StoryObj<TextProps> = {};
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+};
